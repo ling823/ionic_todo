84f10b15ebd44f9fd4de3403d1bbec2cf6f49103
lee <lee19@gmail.com>
2019-11-07 22:03:32 +0800

    first todo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+	todos = [
+	{
+		name:"Learn Ionic",
+		description:"Learn Multiple page",
+		place:"Level 6  VSQ",
+		completed:true
+	},
+	{
+		name:"Do Freecodecamp",
+		description:"Finish Javascript Exercise",
+		place:"Home",
+		completed:false
+	},
+	{
+		name:"Watch movies",
+		description:"Joker",
+		place:"TGV",
+		completed:false
+	}]
+
   constructor() {}
+  markdone(todo){
+
+  }
+  delete(todo){
+  	
+  }
 
 }
